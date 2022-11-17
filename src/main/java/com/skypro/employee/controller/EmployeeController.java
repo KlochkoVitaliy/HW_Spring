46135ba6de7541b62b3b89f08a968af7c5fb0044
VitaliyKlochko <89vklochko89@gmail.com>
2022-11-17 19:35:52 +0300

    Begin++END!! + Lesson_32+WAT

--- src/main/java/com/skypro/employee/controller/EmployeeController.java
+++ src/main/java/com/skypro/employee/controller/EmployeeController.java
@@ -17,7 +17,7 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     public EmployeeController (EmployeeService employeeService){
-        this.employeeService = employeeS ervice;
+        this.employeeService = employeeService;
     }
 
 @GetMapping("/employees")
